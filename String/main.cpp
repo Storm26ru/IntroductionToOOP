dcc81be9a1ae4babf5acf5118bf0fbffbea4af22
Storm <Storm@STORMSSD>
2024-05-22 01:33:26 +0300

    string

--- String/main.cpp
+++ String/main.cpp
@@ -5,13 +5,28 @@ class String
 {
 	char* string;
 	int size=0;
-	void create_string(const char* string, const int size)
+	String& create_string(const char* string, const int size)
 	{
 		this->size = size;
 		this->string = new char[size+1];
 		for (int i = 0; i <= size; i++) this->string[i] = string[i];
+		return *this;
 	}
 public:
+	char* Get_string()const
+	{
+		return string;
+	}
+	int Get_size()const
+	{
+		return size;
+	}
+	void Set_string(const char* string)
+	{
+		delete[] this->string;
+		create_string(string, strlen(string));
+	}
+
 	//					Constructors:
 	String()
 	{
@@ -20,6 +35,11 @@ public:
 		cout << "DefultConstructor" << "\t" << this << endl;
 	}
 	String(const char* string)
+	{
+		create_string(string, strlen(string));
+		cout << "Constructor" << "\t" << this << endl;
+	}
+	String( char* string)
 	{
 		create_string(string, strlen(string));
 		cout << "Constructor" << "\t" << this << endl;
@@ -36,15 +56,33 @@ public:
 		cout << "Destructor" << "\t" << this << endl;
 	}
 
-	//					Methods:
-	char* Get_string()const
+	//					Operator
+	String& operator=(const String& other)
 	{
-		return string;
+		delete[] this->string;
+		create_string(other.string, other.size);
+		cout << "CopyAssigment " << this << endl;
+		return *this;
 	}
-
-	//					Operators:
-	
 };
+
+
+String operator+(const String& left, const String& right)
+{
+	char* bufer = new char[left.Get_size() + right.Get_size()+4];
+	for (int i = 0; i < (left.Get_size() + right.Get_size()+2); i++)
+	{
+		if (i <left.Get_size())
+		{
+			bufer[i] = left.Get_string()[i];
+			//if (bufer[i] == 0)bufer[i] = '.';
+		}
+		else bufer[i] = right.Get_string()[i+1];
+		
+	}
+	return String(bufer);
+	
+}
 
 std::ostream& operator<<(std::ostream& os, const String& obj)
 {
@@ -59,8 +97,9 @@ void main()
 	setlocale(LC_ALL, "");
 
 	String str1 = "Hellow";
-	String str2 = str1;
-	cout << str2<<endl;
+	String str2 = "World";
+	String str3 = str1 + str2;
+	cout << str3<<endl;
 
 
 
